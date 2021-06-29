77e3366ab3c5c314042fe1c69579e5a81b2e55db
FarhanAhmadISM <farhan07.17je003127@me.ism.ac.in>
2021-06-29 21:01:16 +0530

    Add project files.

---
+++ HandGestureRecognition/HandGestureRecognition/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using HandGestureRecognition.SkinDetector;
+
+namespace HandGestureRecognition
+{
+    public partial class Form1 : Form
+    {
+
+        IColorSkinDetector skinDetector;
+
+        Image<Bgr, Byte> currentFrame;
+        Image<Bgr, Byte> currentFrameCopy;
+                
+        Capture grabber;
+        AdaptiveSkinDetector detector;
+        
+        int frameWidth;
+        int frameHeight;
+        
+        Hsv hsv_min;
+        Hsv hsv_max;
+        Ycc YCrCb_min;
+        Ycc YCrCb_max;
+        
+        Seq<Point> hull;
+        Seq<Point> filteredHull;
+        Seq<MCvConvexityDefect> defects;
+        MCvConvexityDefect[] defectArray;
+        Rectangle handRect;
+        MCvBox2D box;
+        Ellipse ellip;
+
+        
+        public Form1()
+        {
+            InitializeComponent(); // the initailisation has taken place
+
+            //grabber = new Emgu.CV.Capture(@".\..\..\..\M2U00253.MPG");   
+            grabber = new Emgu.CV.Capture(@".\..\..\..\M2U00255.mp4"); 
+
+            // For capturing from webcam comment the above 2 lines and uncomment the line below
+            //grabber = new Emgu.CV.Capture(0);
+            // 0 is for default webcam if external webcam is used then instead of 0 there would be 1 or if more webcam is there then check the number and write it inplace of 0 
+            
+            grabber.QueryFrame();
+            frameWidth = grabber.Width;
+            frameHeight = grabber.Height;            
+            detector = new AdaptiveSkinDetector(1, AdaptiveSkinDetector.MorphingMethod.NONE);
+            hsv_min = new Hsv(0, 45, 0); 
+            hsv_max = new Hsv(20, 255, 255);            
+            YCrCb_min = new Ycc(0, 131, 80);
+            YCrCb_max = new Ycc(255, 185, 135);
+            box = new MCvBox2D();
+            ellip = new Ellipse();
+
+            Application.Idle += new EventHandler(FrameGrabber);  // simulaneuosly the process will run                      
+        }
+
+        void FrameGrabber(object sender, EventArgs e)
+        {
+            currentFrame = grabber.QueryFrame();
+            if (currentFrame != null)
+            {
+                currentFrameCopy = currentFrame.Copy();
+                
+                // Uncomment if using opencv adaptive skin detector
+                //Image<Gray,Byte> skin = new Image<Gray,byte>(currentFrameCopy.Width,currentFrameCopy.Height);                
+                //detector.Process(currentFrameCopy, skin);                
+
+                skinDetector = new YCrCbSkinDetector();
+                
+                Image<Gray, Byte> skin = skinDetector.DetectSkin(currentFrameCopy,YCrCb_min,YCrCb_max);
+
+                ExtractContourAndHull(skin);
+
+                imageBoxSkin.Image = skin;
+                imageBoxFrameGrabber.Image = currentFrame;
+            }
+        }
+               
+        private void ExtractContourAndHull(Image<Gray, byte> skin)
+        {
+            using (MemStorage storage = new MemStorage())
+            {
+
+                Contour<Point> contours = skin.FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_LIST, storage);
+                Contour<Point> biggestContour = null;
+
+                Double Result1 = 0;
+                Double Result2 = 0;
+                while (contours != null)
+                {
+                    Result1 = contours.Area;
+                    if (Result1 > Result2)
+                    {
+                        Result2 = Result1;
+                        biggestContour = contours;
+                    }
+                    contours = contours.HNext;
+                }
+
+                if (biggestContour != null)
+                {
+                    //currentFrame.Draw(biggestContour, new Bgr(Color.DarkViolet), 2);
+                    Contour<Point> currentContour = biggestContour.ApproxPoly(biggestContour.Perimeter * 0.0025, storage);
+                    currentFrame.Draw(currentContour, new Bgr(Color.LimeGreen), 2);
+                    biggestContour = currentContour;
+                
+
+                    hull = biggestContour.GetConvexHull(Emgu.CV.CvEnum.ORIENTATION.CV_CLOCKWISE);
+                    box = biggestContour.GetMinAreaRect();
+                    PointF[] points = box.GetVertices();
+                    //handRect = box.MinAreaRect();
+                    //currentFrame.Draw(handRect, new Bgr(200, 0, 0), 1);
+
+                    Point[] ps = new Point[points.Length];
+                    for (int i = 0; i < points.Length; i++)
+                        ps[i] = new Point((int)points[i].X, (int)points[i].Y);
+
+                    currentFrame.DrawPolyline(hull.ToArray(), true, new Bgr(200, 125, 75), 2);
+                    currentFrame.Draw(new CircleF(new PointF(box.center.X, box.center.Y), 3), new Bgr(200, 125, 75), 2);
+
+                    PointF center;
+                    float radius;
+
+
+                    filteredHull = new Seq<Point>(storage);
+                    for (int i = 0; i < hull.Total; i++)
+                    {
+                        if (Math.Sqrt(Math.Pow(hull[i].X - hull[i + 1].X, 2) + Math.Pow(hull[i].Y - hull[i + 1].Y, 2)) > box.size.Width / 10)
+                        {
+                            filteredHull.Push(hull[i]);
+                        }
+                    }
+
+                    defects = biggestContour.GetConvexityDefacts(storage, Emgu.CV.CvEnum.ORIENTATION.CV_CLOCKWISE);
+
+                    defectArray = defects.ToArray();
+                }
+            }
+        }                              
+    }
+}
